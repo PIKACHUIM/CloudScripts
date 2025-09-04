de5da4ea4f57c85a3bd97c0b8e5ab9116c343bdc
pikachuim <pikachuim@qq.com>
2025-09-04 20:09:35 +0800

    add(ci): Debian Server/Graphy/Deepin

--- Linux/Desktop/LXC-Debian-Deepin.sh
+++ Linux/Desktop/LXC-Debian-Deepin.sh
@@ -14,7 +14,7 @@ DEBIAN_FRONTEND=noninteractive apt update && /sbin/init &
 apt install apt-transport-https ca-certificates curl   -y
 DEBIAN_FRONTEND=noninteractive apt install -y pulseaudio 
 SRC="repo.gxde.top/gxde-os/${VER}/g/gxde-source/" 
-wget https://${SRC}gxde-source_1.1.8_all.deb -O gxde.deb 
+wget https://${SRC}gxde-source_1.1.10_all.deb -O gxde.deb 
 dpkg -i gxde.deb && rm -rf gxde.deb && apt install sudo -y
 apt update && apt install -y aptss gxde-testing-source   
 apt install gxde-desktop spark-store --install-recommends -y
