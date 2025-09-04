df551ef68282c8d851b4f7e58d7b472330a4069e
pikachuim <pikachuim@qq.com>
2025-09-04 20:06:55 +0800

    add(ci): Debian Server/Graphy/Deepin

--- Linux/Desktop/LXC-Debian-Deepin.sh
+++ Linux/Desktop/LXC-Debian-Deepin.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 PR="main contrib non-free non-free-firmwar"
+. /etc/os-release
+
+case "${VERSION_ID}" in
+    13) VER="lizhi" ;;
+    12) VER="bixie" ;;
+    *)  echo "Unsupported Debian version: ${VERSION_ID}" >&2; exit 1 ;;
+esac
+export VER
+
 DEBIAN_FRONTEND=noninteractive apt update && /sbin/init & 
 apt install apt-transport-https ca-certificates curl   -y
 DEBIAN_FRONTEND=noninteractive apt install -y pulseaudio 
-if [ $OS_VERSHOW = "13.00" ]; then export VER="lizhi"; fi
-if [ $OS_VERSHOW = "12.00" ]; then export VER="bixie"; fi
 SRC="repo.gxde.top/gxde-os/${VER}/g/gxde-source/" 
 wget https://${SRC}gxde-source_1.1.8_all.deb -O gxde.deb 
 dpkg -i gxde.deb && rm -rf gxde.deb && apt install sudo -y
@@ -20,6 +27,7 @@ nohup x11vnc -forever -noxdamage -repeat -rfbauth \
 EOF
 
 # Startup Desktop ---------------------------------------------
+echo 'echo Starting Desktop Runtime ----------'  >> /run.sh
 echo 'export DISPLAY=:9 &&export $(dbus-launch)' >> /run.sh
 echo 'nohup Xvfb :9 -ac -screen 0 1600x900x24 &' >> /run.sh
 echo 'chmod +x /x11vnc.sh && export HOME=/root ' >> /run.sh
