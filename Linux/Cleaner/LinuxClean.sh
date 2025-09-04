35f9694c1d9724812f79cf7e18f874806a01f1b0
pikachuim <pikachuim@qq.com>
2025-09-04 16:33:06 +0800

    add(ci): LinuxClean

--- Linux/Cleaner/LinuxClean.sh
+++ Linux/Cleaner/LinuxClean.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
-echo "== 备份 & 清理 =="
-sudo tar czpf /var/backups/apt-$(date +%F).tar.gz /var/cache/apt
+echo "============= 清理垃圾 ============="
 > ~/.bash_history
 history -c && history -w
-sudo apt-get clean
-sudo apt-get autoclean
-sudo apt-get autoremove -y
-sudo journalctl --vacuum-time=7d
+apt-get clean
+apt-get autoclean
+apt-get autoremove -y
+journalctl --vacuum-time=7d
 rm -rf ~/.cache/thumbnails/* ~/.local/share/Trash/*
-sudo rm -rf /var/crash/* /var/tmp/*
+rm -rf /var/crash/* /var/tmp/*
 docker system prune -af 2>/dev/null || true
-echo "Done!"
+> ~/.bash_history
+echo "============= 清理完成 ============="
