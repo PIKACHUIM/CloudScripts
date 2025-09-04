3ba97a6d4b9e9a01f83655c45835b435393c1c44
pikachuim <pikachuim@qq.com>
2025-09-04 22:43:14 +0800

    add(ci): Debian Server/Graphy/Deepin

--- Linux/Desktop/LXC-Debian-Deepin.sh
+++ Linux/Desktop/LXC-Debian-Deepin.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+# Check -----------------------------------------------------------
+file="/etc/lxc-ssh-flag"
+if [[ ! -f "$file" ]] || [[ ! -s "$file" ]]; then
+    apt -y install curl && curl https://gh-bat.pika.net.cn/Linux/Desktop/LXC-Debian-Server.sh | bash -e
+	apt -y install curl && curl https://gh-bat.pika.net.cn/Linux/Desktop/LXC-Debian-Graphy.sh | bash -e
+else
+    read -r content < "$file"      # 去掉前后空白，只读第一行
+    case "$content" in
+        0) apt -y install curl && curl https://gh-bat.pika.net.cn/Linux/Desktop/LXC-Debian-Graphy.sh | bash -e ;;
+        9) echo "检查通过，开始安装桌面....." ;;
+        *) echo "已经安装过桌面，禁止重复安装" && exit ;;
+    esac
+fi
 
+# Desktop ------------------------------------------------------
 PR="main contrib non-free non-free-firmwar"
 . /etc/os-release
 
@@ -16,8 +30,9 @@ SRC="repo.gxde.top/gxde-os/${VER}/g/gxde-source/"
 wget https://${SRC}gxde-source_1.1.10_all.deb -O gxde.deb 
 dpkg -i gxde.deb && rm -rf gxde.deb && apt update
 apt install -y gxde-testing-source && apt update 
-apt install gxde-desktop --install-recommends -y
-apt install gxde-desktop-extra firefox-esr spark-store   -y   
+apt install -y  gxde-desktop --install-recommends
+apt install -y  gxde-desktop-extra 
+apt install -y  firefox-esr spark-store    
 
 # X11RDP ------------------------------------------------------ 
 update-alternatives --set x-session-manager /usr/bin/startdde
